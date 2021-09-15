992fffc8241e8c8541eb868a6750b6943a11cf08
b2b-inc <digilabo0601@gmail.com>
2021-09-15 15:25:00 +0900

    second commit

---
+++ blogpost/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import SampleModel
+from .models import BlogModel
+
+# Register your models here.
+admin.site.register(SampleModel)
+admin.site.register(BlogModel)
+
